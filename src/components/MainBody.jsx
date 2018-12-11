c1211513fbdc49f54d328ada39efc44a203b79b1
vlad909 <vladislav.g@cronix.ms>
2018-12-12 02:30:28 +0300

    centered for canvas

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -19,6 +19,48 @@ class MainBody extends React.Component {
         }
     }
 
+    centerImage(img) {
+        var meanX = 0;
+        var meanY = 0;
+        var rows = img.length;
+        var columns = img[0].length;
+        var sumPixels = 0;
+        for (var y = 0; y < rows; y++) {
+            for (var x = 0; x < columns; x++) {
+                var pixel = (1 - img[y][x]);
+                sumPixels += pixel;
+                meanY += y * pixel;
+                meanX += x * pixel;
+            }
+        }
+        meanX /= sumPixels;
+        meanY /= sumPixels;
+
+        var dY = Math.round(rows / 2 - meanY);
+        var dX = Math.round(columns / 2 - meanX);
+        return {transX: dX, transY: dY};
+    }
+
+    getBoundingRectangle(img, threshold) {
+        var rows = img.length;
+        var columns = img[0].length;
+        var minX = columns;
+        var minY = rows;
+        var maxX = -1;
+        var maxY = -1;
+        for (var y = 0; y < rows; y++) {
+            for (var x = 0; x < columns; x++) {
+                if (img[y][x] < threshold) {
+                    if (minX > x) minX = x;
+                    if (maxX < x) maxX = x;
+                    if (minY > y) minY = y;
+                    if (maxY < y) maxY = y;
+                }
+            }
+        }
+        return {minY: minY, minX: minX, maxY: maxY, maxX: maxX};
+    }
+
     imageDataToGrayscale(imgData) {
         var grayscaleImg = [];
         for (var y = 0; y < imgData.height; y++) {
@@ -70,18 +112,19 @@ class MainBody extends React.Component {
         // }
         return (
             <div className="mid">
-                <canvas ref={hidden => this.hiddenCanvas = hidden} width="280" height="280"></canvas>
+                <canvas ref={hidden => this.hiddenCanvas = hidden} width="280" height="280"
+                        style={{display: 'none'}}></canvas>
                 <p className="header-p">Draw here</p>
                 <CanvasDraw className="canvas"
                             brushColor="black"
-                            brushRadius={13}
+                            brushRadius={10}
                             hideGrid={true}
                             style={{
-                    'margin': '10px 50px',
-                    'border': '5px black solid',
-                    width: '280px',
-                    height: '280px'
-                }}
+                                'margin': '10px 50px',
+                                'border': '5px black solid',
+                                width: '280px',
+                                height: '280px'
+                            }}
                             ref={canvasDraw => {
                                 this.saveCanvas = canvasDraw
                             }}/>
@@ -92,6 +135,24 @@ class MainBody extends React.Component {
                     let image = context.getImageData(0, 0,
                         this.saveCanvas.canvas.drawing.width, this.saveCanvas.canvas.drawing.height)
                     let grayscaleImg = this.imageDataToGrayscale(image)
+                    var boundingRectangle = this.getBoundingRectangle(grayscaleImg, 0.01);
+                    var trans = this.centerImage(grayscaleImg)
+                    this.hiddenCanvas.width = image.width
+                    this.hiddenCanvas.height = image.height
+                    let copyCtx = this.hiddenCanvas.getContext('2d')
+                    let brW = boundingRectangle.maxX + 1 - boundingRectangle.minX;
+                    let brH = boundingRectangle.maxY + 1 - boundingRectangle.minY;
+                    let scaling = 190 / (brW > brH ? brW : brH);
+                    // scale
+                    copyCtx.translate(this.saveCanvas.canvas.drawing.width / 2, this.saveCanvas.canvas.drawing.height / 2);
+                    copyCtx.scale(scaling, scaling);
+                    copyCtx.translate(-this.saveCanvas.canvas.drawing.width / 2, -this.saveCanvas.canvas.drawing.height / 2);
+                    // translate to center of mass
+                    copyCtx.translate(trans.transX, trans.transY);
+
+                    copyCtx.drawImage(context.canvas, 0, 0);
+                    image = copyCtx.getImageData(0, 0, 280, 280);
+                    grayscaleImg = this.imageDataToGrayscale(image);
                     // console.log(grayscaleImg, 'gray')
                     // this.setState({byteImage: image})
                     var nnInput = new Array(784), nnInput2 = [];
