45c7bdffcc8f2ecc69a753da94005182ddd4a1be
Vladislav Gushchin <vladislav.g@cronix.ms>
2018-12-12 10:34:15 +0300

    max element + %

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -61,6 +61,76 @@ class MainBody extends React.Component {
         return {minY: minY, minX: minX, maxY: maxY, maxX: maxX};
     }
 
+    knowingImageFromCanvas = () => {
+        // console.log(this.saveCanvas, 'fsd')
+        let context = this.saveCanvas.ctx.drawing
+        let image = context.getImageData(0, 0,
+            this.saveCanvas.canvas.drawing.width, this.saveCanvas.canvas.drawing.height)
+        let grayscaleImg = this.imageDataToGrayscale(image)
+        let boundingRectangle = this.getBoundingRectangle(grayscaleImg, 0.01);
+        let trans = this.centerImage(grayscaleImg)
+        this.hiddenCanvas.width = image.width
+        this.hiddenCanvas.height = image.height
+        let copyCtx = this.hiddenCanvas.getContext('2d')
+        let brW = boundingRectangle.maxX + 1 - boundingRectangle.minX;
+        let brH = boundingRectangle.maxY + 1 - boundingRectangle.minY;
+        let scaling = 190 / (brW > brH ? brW : brH);
+        // scale
+        copyCtx.translate(this.saveCanvas.canvas.drawing.width / 2, this.saveCanvas.canvas.drawing.height / 2);
+        copyCtx.scale(scaling, scaling);
+        copyCtx.translate(-this.saveCanvas.canvas.drawing.width / 2, -this.saveCanvas.canvas.drawing.height / 2);
+        // translate to center of mass
+        copyCtx.translate(trans.transX, trans.transY);
+
+        copyCtx.drawImage(context.canvas, 0, 0);
+        image = copyCtx.getImageData(0, 0, 280, 280);
+        grayscaleImg = this.imageDataToGrayscale(image);
+        // console.log(grayscaleImg, 'gray')
+        // this.setState({byteImage: image})
+        let nnInput = new Array(784), nnInput2 = [];
+        for (let y = 0; y < 28; y++) {
+            for (let x = 0; x < 28; x++) {
+                let mean = 0;
+                for (let v = 0; v < 10; v++) {
+                    for (let h = 0; h < 10; h++) {
+                        mean += grayscaleImg[y * 10 + v][x * 10 + h];
+                    }
+                }
+                mean = (1 - mean / 100); // average and invert
+                nnInput[x * 28 + y] = (mean - .5) / .5;
+            }
+        }
+        let thumbnailCtx = this.smallCanvas.getContext('2d')
+        let thumbnail = thumbnailCtx.getImageData(0, 0, 28, 28);
+        if (true) {
+            context.clearRect(0, 0, this.saveCanvas.canvas.drawing.width, this.saveCanvas.canvas.drawing.height);
+            context.drawImage(this.hiddenCanvas.getContext('2d').canvas, 0, 0);
+            for (let y = 0; y < 28; y++) {
+                for (let x = 0; x < 28; x++) {
+                    let block = context.getImageData(x * 10, y * 10, 10, 10);
+                    let newVal = 255 * (0.5 - nnInput[x * 28 + y] / 2);
+                    nnInput2.push(Math.round((255 - newVal) / 255 * 100) / 100);
+                    for (let i = 0; i < 4 * 10 * 10; i += 4) {
+                        block.data[i] = newVal;
+                        block.data[i + 1] = newVal;
+                        block.data[i + 2] = newVal;
+                        block.data[i + 3] = 255;
+                    }
+                    context.putImageData(block, x * 10, y * 10);
+
+                    thumbnail.data[(y * 28 + x) * 4] = newVal;
+                    thumbnail.data[(y * 28 + x) * 4 + 1] = newVal;
+                    thumbnail.data[(y * 28 + x) * 4 + 2] = newVal;
+                    thumbnail.data[(y * 28 + x) * 4 + 3] = 255;
+                }
+            }
+        }
+        thumbnailCtx.putImageData(thumbnail, 0, 0);
+        this.setState({
+            result: net.run(nnInput2),
+        })
+    }
+
     imageDataToGrayscale(imgData) {
         var grayscaleImg = [];
         for (var y = 0; y < imgData.height; y++) {
@@ -84,6 +154,7 @@ class MainBody extends React.Component {
         return grayscaleImg;
     }
 
+
     componentDidMount() {
         // let net = new brain.NeuralNetwork()
         // net.fromJSON(trainData)
@@ -93,6 +164,16 @@ class MainBody extends React.Component {
         // this.setState({
         //     json: this.state.net.fromJSON(trainData)
         // })
+    }
+
+    renderMax() { //max element index + %
+        let max = Math.max(...this.state.result)
+        return (
+            this.state.result.length ? (
+                <ul>
+                    <li> max:{this.state.result.findIndex(el => el === max)} => {max}%</li>
+                </ul>) : 'Almost empty'
+        )
     }
 
     render() {
@@ -130,76 +211,7 @@ class MainBody extends React.Component {
                                 this.saveCanvas = canvasDraw
                             }}/>
                 {/*<p>byte {this.state.byteImage}</p>*/}
-                <button type="button" onClick={() => {
-                    // console.log(this.saveCanvas, 'fsd')
-                    let context = this.saveCanvas.ctx.drawing
-                    let image = context.getImageData(0, 0,
-                        this.saveCanvas.canvas.drawing.width, this.saveCanvas.canvas.drawing.height)
-                    let grayscaleImg = this.imageDataToGrayscale(image)
-                    let boundingRectangle = this.getBoundingRectangle(grayscaleImg, 0.01);
-                    let trans = this.centerImage(grayscaleImg)
-                    this.hiddenCanvas.width = image.width
-                    this.hiddenCanvas.height = image.height
-                    let copyCtx = this.hiddenCanvas.getContext('2d')
-                    let brW = boundingRectangle.maxX + 1 - boundingRectangle.minX;
-                    let brH = boundingRectangle.maxY + 1 - boundingRectangle.minY;
-                    let scaling = 190 / (brW > brH ? brW : brH);
-                    // scale
-                    copyCtx.translate(this.saveCanvas.canvas.drawing.width / 2, this.saveCanvas.canvas.drawing.height / 2);
-                    copyCtx.scale(scaling, scaling);
-                    copyCtx.translate(-this.saveCanvas.canvas.drawing.width / 2, -this.saveCanvas.canvas.drawing.height / 2);
-                    // translate to center of mass
-                    copyCtx.translate(trans.transX, trans.transY);
-
-                    copyCtx.drawImage(context.canvas, 0, 0);
-                    image = copyCtx.getImageData(0, 0, 280, 280);
-                    grayscaleImg = this.imageDataToGrayscale(image);
-                    // console.log(grayscaleImg, 'gray')
-                    // this.setState({byteImage: image})
-                    let nnInput = new Array(784), nnInput2 = [];
-                    for (let y = 0; y < 28; y++) {
-                        for (let x = 0; x < 28; x++) {
-                            let mean = 0;
-                            for (let v = 0; v < 10; v++) {
-                                for (let h = 0; h < 10; h++) {
-                                    mean += grayscaleImg[y * 10 + v][x * 10 + h];
-                                }
-                            }
-                            mean = (1 - mean / 100); // average and invert
-                            nnInput[x * 28 + y] = (mean - .5) / .5;
-                        }
-                    }
-                    let thumbnailCtx = this.smallCanvas.getContext('2d')
-                    let thumbnail = thumbnailCtx.getImageData(0, 0, 28, 28);
-                    if (true) {
-                        context.clearRect(0, 0, this.saveCanvas.canvas.drawing.width, this.saveCanvas.canvas.drawing.height);
-                        context.drawImage(this.hiddenCanvas.getContext('2d').canvas, 0, 0);
-                        for (let y = 0; y < 28; y++) {
-                            for (let x = 0; x < 28; x++) {
-                                let block = context.getImageData(x * 10, y * 10, 10, 10);
-                                let newVal = 255 * (0.5 - nnInput[x * 28 + y] / 2);
-                                nnInput2.push(Math.round((255 - newVal) / 255 * 100) / 100);
-                                for (let i = 0; i < 4 * 10 * 10; i += 4) {
-                                    block.data[i] = newVal;
-                                    block.data[i + 1] = newVal;
-                                    block.data[i + 2] = newVal;
-                                    block.data[i + 3] = 255;
-                                }
-                                context.putImageData(block, x * 10, y * 10);
-
-                                thumbnail.data[(y * 28 + x) * 4] = newVal;
-                                thumbnail.data[(y * 28 + x) * 4 + 1] = newVal;
-                                thumbnail.data[(y * 28 + x) * 4 + 2] = newVal;
-                                thumbnail.data[(y * 28 + x) * 4 + 3] = 255;
-                            }
-                        }
-                    }
-                    thumbnailCtx.putImageData(thumbnail, 0, 0);
-                    this.setState({
-                        result: net.run(nnInput2),
-                    })
-                    // console.log(net.run(nnInput2))
-                }}>
+                <button type="button" onClick={this.knowingImageFromCanvas}>
                     wat is?
                 </button>
                 <button type="button" onClick={() => {
@@ -208,7 +220,7 @@ class MainBody extends React.Component {
                 </button>
                 <canvas style={{'margin': '10px 50px', 'border': '1px red solid'}}
                         width="28" height="28" ref={small => this.smallCanvas = small}></canvas>
-                {/*{max_el}*/}
+                {this.renderMax()}
                 {result}
             </div>
         )
